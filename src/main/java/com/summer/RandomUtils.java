8d3feb8232f3c4225b3625560a2ad10813ae5068
xia45399 <453993887@qq.com>
2019-01-31 09:32:55 +0800

    randomUtils

--- src/main/java/com/summer/RandomUtils.java
+++ src/main/java/com/summer/RandomUtils.java
@@ -4,18 +4,72 @@ import java.util.Random;
 
 
 public class RandomUtils {
-    private static final String RANDOM_NUM= "0123456789";
-    private static final String RANDOM_STR = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static final String RANDOM_NUM = "0123456789";
+    private static final String RANDOM_UP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String RANDOM_LOW = "abcdefghijklmnopqrstuvwxyz";
+    private static final String RANDOM_NUM_UP = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String RANDOM_NUM_LOW = "0123456789abcdefghijklmnopqrstuvwxyz";
+    private static final String RANDOM_UP_LOW = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static final String RANDOM_ALL = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-    public static String getRandomNum(int length) {
+
+    /**
+     * 随机生成数字类型字符串
+     */
+    public static String randomNum(int length) {
         return getRandomStr(length, RANDOM_NUM);
     }
 
-    public static String getRandomStr(int length) {
-        return getRandomStr(length, RANDOM_STR);
+    /**
+     * 随机生成大写字母类型字符串
+     */
+    public static String randomUp(int length) {
+        return getRandomStr(length, RANDOM_UP);
     }
 
-    public static String getRandomStr(int length, String data) {
+    /**
+     * 随机生成小写字母类型字符串
+     */
+    public static String randomLow(int length) {
+        return getRandomStr(length, RANDOM_LOW);
+    }
+
+    /**
+     * 随机生成字母和大写类型字符串
+     */
+    public static String randomNumAndUp(int length) {
+        return getRandomStr(length, RANDOM_NUM_UP);
+    }
+
+    /**
+     * 随机生成字母和小写类型字符串
+     */
+    public static String randomNumAndLow(int length) {
+        return getRandomStr(length, RANDOM_NUM_LOW);
+    }
+
+    /**
+     * 随机生成大小写类型字符串
+     */
+    public static String randomUpAndLow(int length) {
+        return getRandomStr(length, RANDOM_UP_LOW);
+    }
+
+    /**
+     * 随机生成大小写字母类型字符串
+     */
+    public static String randomAll(int length) {
+        return getRandomStr(length, RANDOM_ALL);
+    }
+
+    /**
+     * 随机生成自定义数据类型字符串
+     */
+    public static String randomData(int length, String data) {
+        return getRandomStr(length, data);
+    }
+
+    private static String getRandomStr(int length, String data) {
         if (data == null) {
             return "";
         }
